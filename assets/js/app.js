2abc83b261c1b7cef58ece4c8747fb770bfdddbb
eddicodes <edw.agyeman@gmail.com>
2020-11-24 22:44:20 +0100

    Updated Button

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,8 @@ fetch(
   "http://data.fixer.io/api/latest?access_key=88600c0e4185a70a4036141ebbfa5eb8"
 )
   .then((response) => response.json())
-//   .then((data) => 
-//   console.log(data.rates.USD))
+  // .then((data) => 
+  // console.log(data.rates.USD))
   .then((data) => {
       button.addEventListener("click", () => {
           output.textContent = (input.value * data.rates.USD).toFixed(2)
