f062f2fd269a0991259db4bb06c607b406ca7b14
eddicodes <edw.agyeman@gmail.com>
2020-11-24 17:08:57 +0100

    TestConversion : EUR -> USD

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,21 @@
-const button = document.querySelector("button")
+const button = document.querySelector("button");
+const input = document.querySelector("#home-currency-amount");
+const output = document.querySelector(".foreign-currency-amount");
 
-button.addEventListener("click", (e) => {
+fetch(
+  "http://data.fixer.io/api/latest?access_key=88600c0e4185a70a4036141ebbfa5eb8"
+)
+  .then((response) => response.json())
+//   .then((data) => 
+//   console.log(data.rates.USD))
+  .then((data) => {
+      button.addEventListener("click", () => {
+          output.textContent = (input.value * data.rates.USD).toFixed(2)
+      })
+  }
+      
+  );
+
+  button.addEventListener("click", (e) => {
     e.target.classList.toggle("rotate")
 })
