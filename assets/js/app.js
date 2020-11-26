05852d3062c58b6c01fa03992b78cf9cd35dac03
eddicodes <edw.agyeman@gmail.com>
2020-11-26 15:54:04 +0100

    Foreign Currency Select Field

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,18 +3,18 @@ const input = document.querySelector("#home-currency-amount");
 const output = document.querySelector(".foreign-currency-amount");
 
 fetch(
-  "http://data.fixer.io/api/latest?access_key=88600c0e4185a70a4036141ebbfa5eb8"
+  "http://data.fixer.io/api/latest?access_key=88600c0e4185a70a4036141ebbfa5eb8&base=usd"
 )
   .then((response) => response.json())
-  // .then((data) => 
-  // console.log(data.rates.USD))
-  .then((data) => {
-      button.addEventListener("click", () => {
-          output.textContent = (input.value * data.rates.USD).toFixed(2)
-      })
-  }
+  .then((data) => 
+  console.log(data))
+  // .then((data) => {
+  //     button.addEventListener("click", () => {
+  //         output.textContent = (input.value * data.rates.USD).toFixed(2)
+  //     })
+  // }
       
-  );
+  // );
 
   button.addEventListener("click", (e) => {
     e.target.classList.toggle("rotate")
