3fb619238b974be2608029047a916959dbd3908a
eddicodes <edw.agyeman@gmail.com>
2020-11-29 13:01:15 +0100

    Foreign Currency Select Field Updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,59 @@
 const button = document.querySelector("button");
 const input = document.querySelector("#home-currency-amount");
-const output = document.querySelector(".foreign-currency-amount");
+const foreignAmount = document.querySelector(".foreign-currency-amount");
+const foreignCode = document.querySelector(".foreign-currency-code")
+const LATEST_RATE_ENDPOINT = "http://data.fixer.io/api/latest?access_key=88600c0e4185a70a4036141ebbfa5eb8"
+const SYMBOLS_ENDPOINT = "http://data.fixer.io/api/symbols?access_key=88600c0e4185a70a4036141ebbfa5eb8"
 
-fetch(
-  "http://data.fixer.io/api/latest?access_key=88600c0e4185a70a4036141ebbfa5eb8&base=usd"
-)
+
+// ====================== OUTPUT OF THE CONVERSION EUR -> X ======================
+fetch(LATEST_RATE_ENDPOINT)
   .then((response) => response.json())
-  .then((data) => 
-  console.log(data))
-  // .then((data) => {
-  //     button.addEventListener("click", () => {
-  //         output.textContent = (input.value * data.rates.USD).toFixed(2)
-  //     })
-  // }
-      
-  // );
+  .then((data) => {
+    console.log(data)
+    button.addEventListener("click", () => {
+      // data.rates.SELECTED CURRENCY instead of USD
+      if(input.value > 0) {
+        foreignAmount.textContent = (input.value * data.rates.USD).toFixed(2);
+      } 
+    });
+  });
 
-  button.addEventListener("click", (e) => {
-    e.target.classList.toggle("rotate")
-})
+ 
+// ====================== ROTATE BUTTON ======================
+
+button.addEventListener("click", (e) => {
+  e.target.classList.toggle("rotate");
+});
+
+
+fetch(LATEST_RATE_ENDPOINT)
+  .then((response) => response.json())
+  .then((data) => {
+    
+    console.log(data);
+  });
+
+
+// Populating Foreign Currency Select Dropdown List
+const dropdown = document.querySelector(".foreign-currency-name");
+dropdown.length = 0;
+
+
+// dropdown.selectedIndex = 0;
+
+
+fetch(SYMBOLS_ENDPOINT)
+  .then((response) => response.json())
+  .then((data) => {
+      for (let el in data.symbols) {
+        let option = document.createElement("option");
+        option.textContent = data.symbols[el];
+        console.log(option.textContent)
+        option.value = data.symbols[el];
+        dropdown.add(option);
+      }
+    });
+
+// foreignCode.textContent = dropdown.selectedIndex
+// console.log(dropdown.selectedIndex)
